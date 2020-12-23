1fc1d0549b41a1082e41fa458b34c114a960ff7a
Peter <mrpeterfulop@gmail.com>
2020-12-23 15:05:59 +0100

    0. update

---
+++ LexiConsole/Global.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LexiConsole
+{
+    public class Global
+        {
+
+            public List<string> myDictionaries = new List<string>();
+            private string DefaultExtension { get; set; }
+            private string DefaultDictionaryPath { get; set; }
+
+            public Global()
+            {
+                SetDefDictionaryPath();
+                SetDefExtension();
+                GetAllDictionaryName();
+            }
+
+            public void GetAllDictionaryName()
+            {
+                var fullpath = GetDefDictionaryPath();
+
+                string[] fileArray = Directory.GetFiles(fullpath);
+                foreach (var item in fileArray)
+                {
+                    myDictionaries.Add(Path.GetFileNameWithoutExtension(item));
+                }
+            }
+
+            public string GetDefExtension()
+            {
+                return DefaultExtension;
+            }
+
+            private void SetDefExtension()
+            {
+                DefaultExtension = ".csv";
+            }
+
+            public string GetDefDictionaryPath()
+            {
+                return DefaultDictionaryPath;
+            }
+
+            private void SetDefDictionaryPath()
+            {
+                DefaultDictionaryPath = Path.GetPathRoot(Environment.SystemDirectory) + @"Users\" + Environment.UserName + @"\Documents\MyLex\Dictionaries\";
+
+                if (!Directory.Exists(DefaultDictionaryPath))
+                {
+                    Directory.CreateDirectory(DefaultDictionaryPath);
+                }
+
+            }
+        }
+    
+}
